2f3642562af6a1e6a6afee4f126f725c819de9db
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-10-09 16:46:29 -0400

    Update main-view.jsx
    added code to fetch the movies from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -19,46 +19,55 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    /* ... */
+    axios.get('https://myflix-api-00001.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
+}
 
-  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+/*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
 
-  setSelectedMovie(movie) {
-    this.setState({
-      selectedMovie: movie
-    });
-  }
+setSelectedMovie(movie) {
+  this.setState({
+    selectedMovie: movie
+  });
+}
 
-  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+/* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
+onLoggedIn(user) {
+  this.setState({
+    user
+  });
+}
 
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+render() {
+  const { movies, selectedMovie } = this.state;
 
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+  /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+  if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    // Before the movies have been loaded
-    if (movies.length === 0) return <div className="main-view" />;
+  // Before the movies have been loaded
+  if (movies.length === 0) return <div className="main-view" />;
 
-    return (
-      <div className="main-view">
-        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
-          ))
-        }
-      </div>
-    );
-  }
+  return (
+    <div className="main-view">
+      {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+      {selectedMovie
+        ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+        ))
+      }
+    </div>
+  );
+}
 
 }
