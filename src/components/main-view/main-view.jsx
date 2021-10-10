cd044111320b4afb9e3c4f97e03e5b03de9fff97
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-10-09 22:28:38 -0400

    Update main-view.jsx
    added reg and login views

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 
+import { RegistrationView } from '../registration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
+
   constructor() {
-    // same code
+    super();
+    // Initial state is set to null
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+      user: null
+    };
   }
 
   componentDidMount() {
@@ -21,17 +30,37 @@ export class MainView extends React.Component {
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
-    // same code
+
+
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+
+  setSelectedMovie(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
   }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
 
   render() {
     const { movies, selectedMovie } = this.state;
 
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
@@ -41,4 +70,5 @@ export class MainView extends React.Component {
       </div>
     );
   }
+
 }
