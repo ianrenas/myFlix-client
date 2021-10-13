651d382800cdcb99c9bb10e5096bef0403d3a0c2
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-10-12 23:33:22 -0400

    Update movie-card.jsx
    Update 10.12

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,7 +15,15 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    }),
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
