7d377f6b023546067c9bdaaa5a44632003fd8f6c
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-10-12 23:33:31 -0400

    Update main-view.jsx
    Update 10.12

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,23 @@
-// myFlix-client/src/main-view/main-view.jsx
-import React from 'react';
 import axios from 'axios';
+import React from 'react';
 
 import { LoginView } from '../login-view/login-view';
-import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../register-view/register-view';
 
 export class MainView extends React.Component {
 
   constructor() {
     super();
-    // Initial state is set to null
     this.state = {
       movies: [],
       selectedMovie: null,
-      user: null
+      user: null,
+      register: null
     };
   }
+
 
   componentDidMount() {
     axios.get('https://myflix-api-00001.herokuapp.com/movies')
@@ -31,43 +31,44 @@ export class MainView extends React.Component {
       });
   }
 
-  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
-
   setSelectedMovie(movie) {
     this.setState({
       selectedMovie: movie
     });
   }
 
-  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
-
-  onLoggedIn(user) {
+  onRegister(register) {
     this.setState({
-      user: user
+      register
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
 
   render() {
-    const { movies, selectedMovie } = this.state;
 
-    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    const { movies, selectedMovie, user, register } = this.state;
 
-    // Before the movies have been loaded
-    if (movies.length === 0) return <div className="main-view" />;
+    if (!register) return <RegistrationView onRegister={register => this.onRegister(register)} />
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+
+
+    if (movies.length === 0) return <div className="main-view"></div>;
 
     return (
       <div className="main-view">
-        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          ? <MovieView movie={selectedMovie} onBackClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
           ))
         }
       </div>
     );
   }
-
 }
