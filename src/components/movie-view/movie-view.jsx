395192e94b3902679fcaec0eea639667592caa03
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-10-12 23:33:11 -0400

    Update movie-view.jsx
    Update 10.12

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -6,6 +6,7 @@ export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
 
+
     return (
       <div className="movie-view">
         <div className="movie-poster">
@@ -19,6 +20,23 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
+        <div className="genre-title">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Title}</span>
+        </div>
+        <div className="genre-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Genre.Description}</span>
+        </div>
+        <div className="director-title">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="director-bio">
+          <span className="label">Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div>
+
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
       </div>
@@ -30,7 +48,14 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired
+    }),
+  }).isRequired
+}
