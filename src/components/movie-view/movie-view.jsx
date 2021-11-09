d2d17b9eaf5fcd60d337eed0625a871c582c33a7
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-11-08 19:32:12 -0500

    Update movie-view.jsx
    Update 11.8.0

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import Badge from 'react-bootstrap/Badge';
+
 
 export class MovieView extends React.Component {
 
+  addFavorite() {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
+
+    axios.post(`https:myflix-api-00001.herokuapp.com/users/${username}/movies/${this.props.movie._id}`, {}, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        alert(`Added to Favorites List`)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   render() {
     const { movie, onBackClick } = this.props;
-
 
     return (
       <div className="movie-view">
@@ -13,49 +33,47 @@ export class MovieView extends React.Component {
           <img src={movie.ImagePath} />
         </div>
         <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
+          <h1>
+            <Badge bg="primary">
+              <span className="value">{movie.Title}</span>
+            </Badge></h1>
         </div>
         <div className="movie-description">
-          <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
-        <div className="genre-title">
-          <span className="label">Genre: </span>
+        <div className="movie-genre">
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">Genre: </Button>
+          </Link>
           <span className="value">{movie.Genre.Name}</span>
         </div>
-        <div className="genre-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Genre.Description}</span>
-        </div>
-        <div className="director-title">
-          <span className="label">Director: </span>
+        <div className="movie-director">
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button variant="link">Director: </Button>
+          </Link>
           <span className="value">{movie.Director.Name}</span>
         </div>
-        <div className="director-bio">
-          <span className="label">Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-
+        <Button variant='danger' className="fav-button" value={movie._id} onClick={(e) => this.addFavorite(e, movie)}>
+          Add to Favorites
+        </Button>
+        <Button variant="primary" onClick={() => { onBackClick(null); }}>Back</Button>
       </div>
     );
   }
 }
+
 
 MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
     Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
+      Name: PropTypes.string.isRequired
     }),
     Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired
+      Name: PropTypes.string.isRequired
     }),
   }).isRequired
-}
+};
