442438278c0ea402c4fa8374e1e6ae7bc5f3007c
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-11-08 19:31:45 -0500

    Update login-view.jsx
    Update 11.8.0

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+
 import './login-view.scss';
 
 export function LoginView(props) {
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /* Send a request to the server for authentication */
+    //Sending a request to the server for authentication, and then call props.onLoggedIn(username) 
     axios.post('https://myflix-api-00001.herokuapp.com/login', {
       Username: username,
       Password: password
@@ -27,37 +30,35 @@ export function LoginView(props) {
 
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-<<<<<<< Updated upstream
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-=======
-        <Form.Control type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)} />
->>>>>>> Stashed changes
-      </Form.Group>
+    <div className="login">
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-<<<<<<< Updated upstream
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-=======
-        <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
->>>>>>> Stashed changes
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Please provide your password</Form.Control.Feedback>
+        </Form.Group>
+        <span>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Log in</Button>
+          {' '}
+          <Link to={`/register`}>
+            <Button variant="success link">Register</Button>
+          </Link>
+        </span>
+      </Form>
+    </div>
   );
 }
 
-
 LoginView.propTypes = {
-  login: PropTypes.shape({
+  user: PropTypes.shape({
     username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-
-  }).isRequired,
+    password: PropTypes.string.isRequired,
+  }),
   onLoggedIn: PropTypes.func.isRequired
 };
